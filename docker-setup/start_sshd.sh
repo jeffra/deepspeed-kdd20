1faed4aa561c44f0340e7a1d3939ac58834b5f59
Jeff Rasley <jerasley@microsoft.com>
2020-07-22 15:02:35 -0700

    allow different ssh ports

--- docker-setup/start_sshd.sh
+++ docker-setup/start_sshd.sh
@@ -1,8 +1,7 @@
 SSH_PORT=2222
 
-cat /etc/ssh/sshd_config > sshd_config 
-sed -i "0,/^#Port 22/s//Port ${SSH_PORT}/" sshd_config 
+cat /etc/ssh/sshd_config > sshd_config
+sed -i "0,/^Port [0-9]\+/s//Port ${SSH_PORT}/" sshd_config
 sudo cp sshd_config /etc/ssh/sshd_config
 
 sudo service ssh start
-
