affcda0ede76ed07df75b3659a10d1046da4106c
Jeff Rasley <jerasley@microsoft.com>
2020-07-23 14:23:04 -0700

    mv ssh config

--- docker-setup/4_start_sshd.sh
+++ docker-setup/4_start_sshd.sh
@@ -4,7 +4,7 @@ SSH_PORT=2222
 
 cat /etc/ssh/sshd_config > sshd_config
 sed -i "0,/^Port [0-9]\+/s//Port ${SSH_PORT}/" sshd_config
-sudo cp sshd_config /etc/ssh/sshd_config
+sudo mv sshd_config /etc/ssh/sshd_config
 
 sudo service ssh start
 sleep 1
