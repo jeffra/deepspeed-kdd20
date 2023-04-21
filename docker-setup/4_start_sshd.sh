942fb3c4117fc5a4f6d63b247b17f60cdf11e781
Wang Sheng  Face No1 <wustwangsheng@gmail.com>
2023-04-21 14:07:08 +0800

    Update 4_start_sshd.sh

--- docker-setup/4_start_sshd.sh
+++ docker-setup/4_start_sshd.sh
@@ -4,6 +4,7 @@ SSH_PORT=2222
 
 cat /etc/ssh/sshd_config > sshd_config
 sed -i "0,/^Port [0-9]\+/s//Port ${SSH_PORT}/" sshd_config
+sed -i "0,/^#Port [0-9]\+/s//Port ${SSH_PORT}/" sshd_config
 sudo mv sshd_config /etc/ssh/sshd_config
 
 sudo service ssh start
